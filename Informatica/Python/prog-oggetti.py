efe7017fd6d253d84a0cb8725350907a2c3b9945
thomas-addamo <thomas.addamo2@jcmaxwell.it>
2025-09-18 08:45:38 +0200

    introduzione alla programmazione oggetti

---
+++ Informatica/Python/prog-oggetti.py
@@ -0,0 +1,6 @@
+#Programmazione ad oggetti
+#fino ad ora abbiamo programmato a modo imperativo
+
+#la programmazione ad oggetti è un paradigma di programmazione che utilizza "oggetti" e le loro interazioni per progettare applicazioni e programmi informatici.
+#un oggetto è una struttura dati che contiene sia dati (attributi) che funzioni (metodi) che operano su quei dati.
+
