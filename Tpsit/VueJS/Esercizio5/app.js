e95dd634aebf59ac64f8fd58f841521bfccbc4a8
thomas-addamo <thomas.addamo2@jcmaxwell.it>
2025-11-01 23:52:31 +0100

    Add user greeting and new Vue components
    Introduced a user greeting feature with input and button to display a personalized message. Added two new Vue components: 'info-card' and 'saluto-utente', and updated the HTML to use these components.

--- Tpsit/VueJS/Esercizio5/app.js
+++ Tpsit/VueJS/Esercizio5/app.js
@@ -7,7 +7,26 @@ createApp({
             isSuccess: false,
             coloreAvatar: '#ff0000',
             dimensioneAvatar: 50,
-            larghezza: 50
+            larghezza: 50,
+            nomeUtente: '',
+            messaggioSaluto: ''
+        }
+    },
+    components: {
+        'info-card': {
+            template: `
+                <div class="box">
+                    <h3>Titolo della Card</h3>
+                    <p>Contenuto della card...</p>
+                </div>
+            `
+        },
+        'saluto-utente': {
+            template: `
+                <div class="box">
+                    <p>Benvenuto nella nostra applicazione</p>
+                </div>
+            `
         }
     },
     methods: {
@@ -18,6 +37,13 @@ createApp({
         messaggioSuccesso() {
             this.isSuccess = true;
             this.isError = false;
+        },
+        salutaUtente() {
+            if (this.nomeUtente.trim() !== '') {
+                this.messaggioSaluto = `Ciao, ${this.nomeUtente}! Benvenuto!`;
+            } else {
+                this.messaggioSaluto = 'Per favore, inserisci il tuo nome.';
+            }
         }
     }
 }).mount('#app');
